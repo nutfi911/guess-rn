711192c4a7605740526407df203e403682aa362f
Nutfi <nutfi911@icloud.com>
2022-01-27 20:45:11 +0200

    Main screen title added

---
+++ screens/StartGame.js
@@ -0,0 +1,14 @@
+import { View, StyleSheet, Text } from "react-native";
+
+const StartGame = (props) => {
+    return <View style={styles.screen}></View>;
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+    },
+});
+
+export default StartGame;
